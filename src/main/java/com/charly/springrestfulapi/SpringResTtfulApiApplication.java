db7a6207a178b2ce50231a0e942ad19fffad3c28
Charly Butarbutar <charlymicolas21@gmail.com>
2023-05-17 23:37:49 +0700

    feat: init project

---
+++ src/main/java/com/charly/springrestfulapi/SpringResTtfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.charly.springrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTtfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResTtfulApiApplication.class, args);
+	}
+
+}
